80a32291e8c0bb8e467591e6b262b72848582be8
Nafis Saami Azad <nafissazad@gmail.com>
2023-07-17 02:32:55 +0600

    Implemented logout

--- react/src/layouts/DefaultLayout.jsx
+++ react/src/layouts/DefaultLayout.jsx
@@ -1,8 +1,10 @@
 import {Link, Navigate, Outlet} from "react-router-dom";
 import {useStateContext} from "../contexts/ContextProvider.jsx";
+import {useEffect} from "react";
+import axiosClient from "../axios.jsx";
 
 function DefaultLayout(props) {
-    const {user, token} = useStateContext()
+    const {user, token, setUser, setToken} = useStateContext()
 
     if (!token) {
         return <Navigate to="/login"/>
@@ -10,7 +12,18 @@ function DefaultLayout(props) {
 
     const onLogout = (ev) => {
         ev.preventDefault();
-    };
+
+        axiosClient.post("/logout").then(() => {
+            setUser({})
+            setToken(null)
+        })
+    }
+
+    useEffect(() => {
+        axiosClient.get("/user").then(({data}) => {
+            setUser(data);
+        });
+    }, []);
 
     return (
         <div id="defaultLayout">
--- react/src/views/Login.jsx
+++ react/src/views/Login.jsx
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, {createRef, useState} from 'react';
 import {Link} from "react-router-dom";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
+import axiosClient from "../axios.jsx";
 
 function Login() {
 
+    const emailRef = createRef()
+    const passwordRef = createRef()
+    const {setUser, setToken} = useStateContext()
+    const [message, setMessage] = useState(null)
     const onSubmit = (ev) => {
         ev.preventDefault()
+
+        const payload = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value
+        }
+
+        axiosClient.post("/login", payload).then(({data}) => {
+            setUser(data.user)
+            setToken(data.token)
+        }).catch((error) => {
+            const response = error.response
+            if (response && response.status === 422) {
+                setMessage(response.data.message)
+            }
+        })
     }
+
     return (
         <div className="login-signup-form animated fadeInDown">
             <div className="form">
                 <form onSubmit={onSubmit}>
                     <h1 className="title">Log into your account</h1>
-                    <input type="email" placeholder="Email"/>
-                    <input type="password" placeholder="Password"/>
+                    {message && (
+                        <div className="alert">
+                            <p>{message}</p>
+                        </div>
+                    )}
+                    <input ref={emailRef} type="email" placeholder="Email"/>
+                    <input ref={passwordRef} type="password" placeholder="Password"/>
                     <button className="btn btn-block">Login</button>
                     <p className="message">
                         Not Registered? <Link to="/signup">Create an account</Link>
@@ -20,7 +47,6 @@ function Login() {
 
                 </form>
             </div>
-
         </div>
     );
 }
